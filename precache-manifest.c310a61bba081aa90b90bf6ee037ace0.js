95a0d30b407fb159c6fe18ffc9062c68270033ef
Maryna <shpakmaryna2014@gmail.com>
2019-10-20 13:32:22 +0300

    Updates

---
+++ precache-manifest.c310a61bba081aa90b90bf6ee037ace0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfb18e97579c605a29e302babbaf4fcc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa6bf41dac238e5edabc",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "c3b94be310bbecd7a07c",
+    "url": "/static/js/2.1a93609f.chunk.js"
+  },
+  {
+    "revision": "aa6bf41dac238e5edabc",
+    "url": "/static/js/main.a12bdab2.chunk.js"
+  },
+  {
+    "revision": "f342ecb9eb1f9a22b13f",
+    "url": "/static/js/runtime-main.cedaa9fa.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
